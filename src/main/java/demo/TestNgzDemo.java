af68ad797aac127721856b7e1c85666cdb9bd93c
Mithun Reddy <user@Mithuns-Mac.local>
2023-08-23 23:43:02 +0530

    fjg

--- src/main/java/demo/TestNgzDemo.java
+++ src/main/java/demo/TestNgzDemo.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,13 +25,16 @@ public class TestNgzDemo {
 	}
 	
 	@Test ()
+	public void search5() {
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Selenium");
+		search.submit();
+		System.out.println("Method 1");
+	}
+	@Test ()
 	public void search2() {
 		System.out.println("Method 2");
 	}
 	
-	@AfterMethod
-	public  void closeBrowser() {
-		driver.close();
-	}
 
 }
