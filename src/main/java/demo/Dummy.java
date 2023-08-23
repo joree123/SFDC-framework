1db2e8a2188bb28a906ed1c23b505fa629362b60
Mithun Reddy <user@Mithuns-Mac.local>
2023-08-23 22:22:37 +0530

    some more tests

--- src/main/java/demo/Dummy.java
+++ src/main/java/demo/Dummy.java
@@ -1,5 +1,33 @@
 package demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
 public class Dummy {
+	
+	@Test ()
+	public void search2() {
+//		WebElement search = driver.findElement(By.name("q"));
+//		search.sendKeys("Selenium");
+//		search.submit();
+//		System.out.println("Method 1");
+	}
+	
+	@Test ()
+	public void search3() {
+		System.out.println("Method 2");
+	}
+	
+	@Test ()
+	public void search4() {
+		System.out.println("Method 2");
+	}
+	
+//	@AfterMethod
+//	public  void closeBrowser() {
+//		driver.close();
+//	}
 
 }
